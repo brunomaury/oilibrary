29e0e022716172b67907534e70d820441920fd1f
brunomaury <bruno.maury@optimistik.com>
2025-06-06 09:14:13 +0200

    chore: setup basic structure

---
+++ src/main/java/fr/bruno/oilibrary/OilibraryApplication.java
@@ -0,0 +1,13 @@
+package fr.bruno.oilibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OilibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/bruno/oilibrary/OilibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bruno.oilibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
