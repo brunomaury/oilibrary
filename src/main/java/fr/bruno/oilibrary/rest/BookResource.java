9b698f7078c0a55dfe2e537c57819ad2d36c0f3e
brunomaury <bruno.maury@optimistik.com>
2025-06-24 16:42:38 +0200

    nit(rest): remove useless import

--- src/main/java/fr/bruno/oilibrary/rest/BookResource.java
+++ src/main/java/fr/bruno/oilibrary/rest/BookResource.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * This class provides RESTful web service endpoints for performing operations
