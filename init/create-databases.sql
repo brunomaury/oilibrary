b072db70d903702c529bc68af831e248e356f19a
brunomaury <bruno.maury@optimistik.com>
2025-07-22 20:46:04 +0200

    feat(testing):setup to manage dev/test profiles

---
+++ init/create-databases.sql
@@ -0,0 +1,7 @@
+create user "oilibrary-dev" with password 'oilibrary-dev';
+create database "oilibrary-dev";
+Alter Database "oilibrary-dev" Owner To "oilibrary-dev";
+
+create user "oilibrary-test" with password 'oilibrary-test';
+create database "oilibrary-test";
+Alter Database "oilibrary-test" Owner To "oilibrary-test";
