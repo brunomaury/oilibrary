2b69c2ec2a91c032a0bd459dd59ed8a535ab8b04
brunomaury <bruno.maury@optimistik.com>
2025-08-04 10:36:31 +0200

    feat(testing): Update project structure to test (separate DB)

--- init/create-databases.sql
+++ init/create-databases.sql
@@ -5,3 +5,6 @@ Alter Database "oilibrary-dev" Owner To "oilibrary-dev";
 create user "oilibrary-test" with password 'oilibrary-test';
 create database "oilibrary-test";
 Alter Database "oilibrary-test" Owner To "oilibrary-test";
+
+GRANT ALL ON "oilibrary-dev" To "oilibrary-dev";
+GRANT ALL ON "oilibrary-test" To "oilibrary-test";
